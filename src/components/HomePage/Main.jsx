d5494f57cf7b5384f043b5c9af0b84fa38ac4c6b
gone-keerthi <keerthigone2002@gmail.com>
2023-12-02 13:54:53 +0530

    updater url

--- src/components/HomePage/Main.jsx
+++ src/components/HomePage/Main.jsx
@@ -16,7 +16,7 @@ const Main = () => {
   const navigate = useNavigate();
   const openGitRepo = () => {
     // Replace 'YOUR_GIT_REPO_URL' with the actual URL of your Git repository
-    window.open("", "_blank");
+    window.open("https://github.com/gone-keerthi/Zevi-App-", "_blank");
   };
 
   const openAssignmentPdf = () => {
