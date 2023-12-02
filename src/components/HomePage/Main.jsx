04afd3d1d5df0fecde3d2134685cf56b02a84b54
gone-keerthi <keerthigone2002@gmail.com>
2023-12-02 14:42:42 +0530

    file extension has changed

--- src/components/HomePage/Main.jsx
+++ src/components/HomePage/Main.jsx
@@ -16,7 +16,7 @@ const Main = () => {
   const navigate = useNavigate();
   const openGitRepo = () => {
     // Replace 'YOUR_GIT_REPO_URL' with the actual URL of your Git repository
-    window.open("https://github.com/gone-keerthi/Zevi-App-", "_blank");
+    window.open("https://github.com/gone-keerthi/Zevi-App-12", "_blank");
   };
 
   const openAssignmentPdf = () => {
